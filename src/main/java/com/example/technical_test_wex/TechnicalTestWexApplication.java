f765d4ed922fb2605580481af3cdb716ea91e990
Thiago Soares <thiagobellinics@hotmail.com>
2024-11-04 19:39:26 -0300

    First commit

---
+++ src/main/java/com/example/technical_test_wex/TechnicalTestWexApplication.java
@@ -0,0 +1,13 @@
+package com.example.technical_test_wex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestWexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestWexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/technical_test_wex/TechnicalTestWexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technical_test_wex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestWexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
